d9756c3bdd9bca0fc961f274dfa160de9d521260
kasturi28 <kasturipansare2818@gmail.com>
2022-08-15 16:57:47 +0530

    Added UC1_ Validation of last name

--- src/practiseproblem/RegexProgram.java
+++ src/practiseproblem/RegexProgram.java
@@ -4,16 +4,24 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class RegexProgram {
-
+/**
+ * 
+ * @param args
+ */
 	public static void main(String[] args) {
-
-		System.out.println("Enter First Name");
+/*
+ * Taking input from the user for last name using scanner function
+ * Defining result & pattern for the program 
+ */
+		System.out.println("Enter last Name");
 		Scanner sc = new Scanner(System.in);
 		String sampleName = sc.nextLine();
 
-		// boolean result = Pattern.matches("Regex Pattern", sampleName);
+		
 		boolean result = Pattern.matches("[A-Z]{1}[a-z]{2,}", sampleName);
-
+/*
+ * Printing the results
+ */
 		System.out.println("Result = " + result);
 
 	}
